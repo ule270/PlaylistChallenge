a4f159dc32fee5bfded48eeee71811dd7f168924
ule270 <u.le270@gmail.com>
2023-04-04 17:40:45 -0400

    completed.

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Music {
 
     private String[] playList;
@@ -9,6 +12,10 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        ArrayList<String> musicSel = new ArrayList<String>(Arrays.asList(this.playList));
+        int forwardCount = musicSel.indexOf(selection) - startIndex;
+        int backwardCount = musicSel.size() + startIndex - musicSel.lastIndexOf(selection);
+        // use build in function that returns minimum of two numbers.
+        return Math.min(forwardCount, backwardCount);
     }
 }
